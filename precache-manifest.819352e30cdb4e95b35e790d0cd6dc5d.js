0e331b4d58ae1f1261f0befec7931161770eff54
Mickanie <michalina.kaniewska@gmail.com>
2019-05-29 20:05:47 +0200

    Updates

--- precache-manifest.819352e30cdb4e95b35e790d0cd6dc5d.js
+++ precache-manifest.819352e30cdb4e95b35e790d0cd6dc5d.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "29791a39df74d78532b40b1d7b28bd2b",
+    "revision": "7bd0d38407ebc4318fcb8d08ad9ad42d",
     "url": "/Book-Tracker/index.html"
   },
   {
-    "revision": "d79a7acf7d0f14c01d5a",
-    "url": "/Book-Tracker/static/css/main.b10f75bf.chunk.css"
+    "revision": "65e1f7b85c541773699c",
+    "url": "/Book-Tracker/static/css/main.f4a2e572.chunk.css"
   },
   {
     "revision": "ddd4e623390ec3facaf3",
     "url": "/Book-Tracker/static/js/2.77b90360.chunk.js"
   },
   {
-    "revision": "d79a7acf7d0f14c01d5a",
+    "revision": "65e1f7b85c541773699c",
     "url": "/Book-Tracker/static/js/main.7f99bacc.chunk.js"
   },
   {
