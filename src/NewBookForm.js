ea4a5be813c4ecbded8868aeca661ad5e6529b1a
Mickanie <michalina.kaniewska@gmail.com>
2019-05-29 13:52:47 +0200

    Create form to add a book

--- src/NewBookForm.js
+++ src/NewBookForm.js
@@ -25,6 +25,35 @@ class NewBookForm extends Component {
     return (
       <div className="new-book-container">
         <h3>Add a new book</h3>
+        <form>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" pattern="[A-Za-z\s0-9]{3,45}" />
+          <label htmlFor="author">Author</label>
+          <input
+            type="text"
+            name="author"
+            pattern="[A-Za-z]{3,15}\s{1}[A-Za-z]{3,20}"
+          />
+          <label htmlFor="ISBN">ISBN</label>
+          <input type="text" name="ISBN" pattern="[0-9]{13}" />
+          <label htmlFor="pages">No of pages</label>
+          <input type="text" name="pages" pattern="[0-9]{2,4}" />
+          <label htmlFor="rating">Your rating</label>
+          <div className="rating-stars">
+            <input type="radio" name="rating" value="1" id="rate-1" />
+            <label htmlFor="rate-1">★</label>
+            <input type="radio" name="rating" value="2" id="rate-2" />
+            <label htmlFor="rate-2">★</label>
+            <input type="radio" name="rating" value="3" id="rate-3" />
+            <label htmlFor="rate-3">★</label>
+            <input type="radio" name="rating" value="4" id="rate-4" />
+            <label htmlFor="rate-4">★</label>
+            <input type="radio" name="rating" value="5" id="rate-5" />
+            <label htmlFor="rate-5">★</label>
+          </div>
+
+          <input type="submit" value="Add" />
+        </form>
       </div>
     );
   }
